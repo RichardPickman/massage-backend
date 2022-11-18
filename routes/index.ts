c6b17260db66ad75daaa526f315f3fa02d22a84d
RichardPickman <drezzerock@gmail.com>
2022-11-18 13:16:20 +0100

    feat: add question route

--- routes/index.ts
+++ routes/index.ts
@@ -2,11 +2,13 @@ import Router from "express";
 import userRouter from "./userRouter";
 import quizRouter from "./quizRouter";
 import lectureRouter from "./lectureRouter";
+import questionRouter from "./questionRouter";
 
 const router = Router();
 
 router.use("/users", userRouter);
 router.use("/quiz", quizRouter);
 router.use("/lectures", lectureRouter);
+router.use("/question", questionRouter);
 
 export default router;
