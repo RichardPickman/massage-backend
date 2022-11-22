81dd9275f91ddf62eeade65468d7733726cbdb23
RichardPickman <drezzerock@gmail.com>
2022-11-22 13:13:13 +0100

    refactor: change quotes

--- index.ts
+++ index.ts
@@ -1,17 +1,16 @@
-import express, { json } from 'express';
-import cors from 'cors';
-import { getServerConfig } from './config';
-import router from './routes';
-import errorHandler from './middleware/ErrorHandle/index'
-import './model/db';
-
+import express, { json } from "express";
+import cors from "cors";
+import { getServerConfig } from "./config";
+import router from "./routes";
+import errorHandler from "./middleware/ErrorHandle/index";
+import "./model/db";
 
 const { port } = getServerConfig();
 const app = express();
 
 app.use(cors());
 app.use(json());
-app.use('/api', router);
-app.use(errorHandler)
+app.use("/api", router);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
