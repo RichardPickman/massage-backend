4b4ab57ca1a6b58d992d9baf2b95dfeb48b9a931
RichardPickman <drezzerock@gmail.com>
2022-12-26 17:51:25 +0100

    feat: add few functions for user service

--- services/User.ts
+++ services/User.ts
@@ -1,33 +1,118 @@
-import ApiError from "../Error";
+import ApiError from "../exceptions";
 import userModel from "../model/schemes/User";
-
+import bcrypt from "bcrypt";
+import { v4 as uuidv4 } from "uuid";
+import MailService from "../controllers/mailController";
+import TokenService from "../controllers/tokenController";
+import UserDto from "../dtos/user";
 
 class UserService {
-    async create({ user, email, password, role }: { [k: string]: string }) {
-        const createUser = await new userModel(({ user, email, role, password }))
-        .save()
-        .then(() => this.find({ email }));
+  async registration(email: string, password: string) {
+    const candidate = await userModel.findOne({ email });
 
-        return createUser;
+    if (candidate) {
+      console.log("UserService: candidate already found");
+
+      return;
     }
 
-    async delete(id: string) {
-        const removeItem = await userModel.deleteOne({ _id: id });
+    const hashPassword = await bcrypt.hash(password, 3);
+    const activationLink = uuidv4();
 
-        return removeItem;
+    const user = await userModel.create({
+      email,
+      password: hashPassword,
+      activationLink,
+    });
+    await MailService.sendActivationLink(
+      email,
+      `${process.env.API_URL}/api/users/activate/${activationLink}`
+    );
+
+    const userDto = new UserDto(user);
+    const tokens = TokenService.generateTokens({ ...userDto });
+    await TokenService.saveToken(userDto.id, tokens.refreshToken);
+
+    return {
+      ...tokens,
+      user: userDto,
+    };
+  }
+
+  async activate(activationLink: string) {
+    const user = await userModel.findOne({ activationLink });
+
+    if (!user) {
+      console.log("There is no such user");
+
+      return;
     }
 
-    async update({ user, email, password }: { [k: string]: string }) {
-        const createUser = new userModel({ user, email, password }).save();
+    user.isActivated = true;
 
-        return createUser;
+    await user.save();
+  }
+
+  async login(email, password) {
+    const userData = await userModel.findOne({ email });
+
+    if (!userData) {
+      throw ApiError.BadRequest("User not found");
     }
 
-    async find({ email }: { [k: string]: string }) {
-        const user = userModel.findOne({ email });
+    const isPassEquals = await bcrypt.compare(password, userData.password);
 
-        return user;
+    if (!isPassEquals) {
+      throw ApiError.BadRequest("Invalid email or password");
     }
+
+    const userDto = new UserDto(userData);
+    const tokens = TokenService.generateTokens({ ...userDto });
+    await TokenService.saveToken(userDto.id, tokens.refreshToken);
+
+    return {
+      ...tokens,
+      user: userDto,
+    };
+  }
+
+  async logout(refreshToken) {
+    try {
+      const token = await TokenService.removeToken(refreshToken);
+
+      return token;
+      return;
+    } catch (e) {}
+  }
+
+  async refresh(refreshToken) {
+    if (!refreshToken) {
+      throw ApiError.UnauthorizedError();
+    }
+
+    const userData = TokenService.validateRefreshToken(refreshToken);
+    const tokenFromDB = TokenService.findToken(refreshToken);
+
+    if (!userData || !tokenFromDB) {
+      throw ApiError.UnauthorizedError();
+    }
+
+    const user = await userModel.findById(userData.id);
+    const userDto = new UserDto(user);
+    const tokens = TokenService.generateTokens({ ...userDto });
+    await TokenService.saveToken(userDto.id, tokens.refreshToken);
+
+    return {
+      ...tokens,
+      user: userDto,
+    };
+  }
+
+  async getAll() {
+    const users = await userModel.find();
+
+    return users;
+  }
 }
 
 const User = new UserService();
