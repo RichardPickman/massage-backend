e215583cba5ecc6106654d032cc4b6bad0c32aca
RichardPickman <drezzerock@gmail.com>
2022-12-26 17:51:37 +0100

    feat: add routes for user

--- routes/userRouter.ts
+++ routes/userRouter.ts
@@ -1,17 +1,24 @@
 import userController from "../controllers/userController";
 import JwtMiddleware from "../middleware/Token";
 import checkRole from "../middleware/Role";
+import { body } from "express-validator";
+import authMiddleware from "../middleware/auth";
 
 const user = new userController();
 const e = require("express");
 
 const router = e();
 
-router.post("/registration", user.registration);
+router.post(
+  "/registration",
+  body("email").isEmail(),
+  body("password").isLength({ min: 6, max: 32 }),
+  user.registration
+);
 router.post("/login", user.login);
-
-router.get("/auth", JwtMiddleware, checkRole("admin"), user.check);
-
-router.delete("/remove/:id", JwtMiddleware, checkRole("admin"), user.remove);
+router.post("/logout", user.logout);
+router.get("/activate/:link", user.activate);
+router.get("/refresh", user.refresh);
+router.get("/all", authMiddleware, user.getAll);
 
 export default router;
