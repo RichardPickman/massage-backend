0cfa04e26afe6824876b8c484431186f8249ac3c
RichardPickman <drezzerock@gmail.com>
2022-12-26 17:51:49 +0100

    feat: add cookieparser and cors settings

--- index.ts
+++ index.ts
@@ -1,16 +1,23 @@
 const cors = require("cors");
 import { getServerConfig } from "./config";
 import router from "./routes";
-import errorHandler from "./middleware/ErrorHandle/index";
+import errorHandler from "./middleware/ErrorHandle";
 import logger from "./middleware/log/index";
 import "./model/db";
-const e = require("express");
+import cookieParser from "cookie-parser";
 
+const e = require("express");
 const { port } = getServerConfig();
 const app = e();
 
-app.use(cors());
 app.use(e.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "*",
+    credetials: true,
+  })
+);
 app.use(logger);
 app.use("/api", router);
 app.use(errorHandler);
