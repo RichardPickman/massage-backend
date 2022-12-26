abd3526cf210cf97245255fc9b3365afda55bc6c
RichardPickman <drezzerock@gmail.com>
2022-12-26 17:49:57 +0100

    refactor: add errors array

--- middleware/ErrorHandle/index.ts
+++ middleware/ErrorHandle/index.ts
@@ -1,9 +1,11 @@
-import ApiError from "../../Error";
+import ApiError from "../../exceptions";
 
 export default (err: any, req: any, res: any, next: any) => {
   if (err instanceof ApiError) {
-    return res.status(err.status).json({ message: err.message });
+    return res
+      .status(err.status)
+      .json({ message: err.message, errors: err.errors });
   }
 
-  return res.status(500).json({ message: err });
+  return res.status(500).json({ message: "Unhandled error" });
 };
