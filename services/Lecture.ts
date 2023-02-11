963f7aadbbb44dfff8ac8a75d8407fc567e6be0d
RichardPickman <drezzerock@gmail.com>
2023-02-11 20:42:51 +0100

    feat: add plain text ids and population

--- services/Lecture.ts
+++ services/Lecture.ts
@@ -1,8 +1,16 @@
 import lectureModel from "../model/schemes/Lecture";
+import TeacherService from "../services/Teacher";
+import LessonService from "../services/Lesson";
 
 class LectureService {
   async create(props: Record<any, any>) {
-    const createLecture = await new lectureModel(props)
+    const teacher = await TeacherService.get(props.teacher);
+    const lesson = await LessonService.get(props.lesson);
+    const createLecture = await new lectureModel({
+      ...props,
+      teacher: teacher.id,
+      lesson: lesson.id,
+    })
       .save()
       .then((item) => this.find(item.id));
 
@@ -16,13 +24,19 @@ class LectureService {
   }
 
   async find(id: string) {
-    const lecture = lectureModel.findOne({ _id: id });
+    const lecture = lectureModel
+      .findOne({ _id: id })
+      .populate("teacher")
+      .populate("lesson");
 
     return lecture;
   }
 
   async findAll() {
-    const lectures = await lectureModel.find({});
+    const lectures = await lectureModel
+      .find({})
+      .populate("teacher")
+      .populate("lesson");
 
     return lectures;
   }
