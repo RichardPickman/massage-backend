b462c9158bcde1dee0bd09d10eb64ba1338c94df
RichardPickman <drezzerock@gmail.com>
2023-02-11 20:40:27 +0100

    refactor: enforce to string

--- middleware/auth/index.ts
+++ middleware/auth/index.ts
@@ -4,13 +4,14 @@ import TokenService from "../../controllers/tokenController";
 
 export default function (req: Request, res: Response, next: NextFunction) {
   try {
-    const authorizationHeader = req.headers.authorization;
+    const authorizationHeader = req.headers.authorization as string;
 
     if (!authorizationHeader) {
       return next(ApiError.UnauthorizedError());
     }
 
     const accessToken = authorizationHeader.split(" ")[1];
+
     if (!accessToken) {
       return next(ApiError.UnauthorizedError());
     }
