f4202d093c15372fbd2955013bc7ea0d2f02a018
RichardPickman <drezzerock@gmail.com>
2023-02-11 20:42:31 +0100

    feat: add auth verification and METHOD's update

--- routes/quizRouter.ts
+++ routes/quizRouter.ts
@@ -1,14 +1,15 @@
 import QuizController from "../controllers/quizController";
+import authMiddleware from "../middleware/auth";
 
 const quiz = new QuizController();
 const e = require("express");
 
 const router = e();
 
-router.post("/create", quiz.create);
+router.post("/create", authMiddleware, quiz.create);
 router.get("/all", quiz.getAll);
-router.put("/update/:id", quiz.update);
-router.post("/remove/:id", quiz.remove);
+router.put("/update/:id", authMiddleware, quiz.update);
+router.delete("/remove/:id", authMiddleware, quiz.remove);
 router.get("/:id", quiz.get);
 
 export default router;
