c769b9be777c413f61ed7fc5a413882f8d7b8e94
RichardPickman <drezzerock@gmail.com>
2023-02-11 20:40:09 +0100

    feat: add new argument

--- helpers/index.ts
+++ helpers/index.ts
@@ -5,11 +5,10 @@ import * as crypto from "crypto";
 export const getFormattedBuffer = async (
   path: string,
   width = 1920,
-  height = 1080
+  height = 1080,
+  fit = "contain"
 ) =>
-  sharp(fs.readFileSync(path))
-    .resize({ height, width, fit: "contain" })
-    .toBuffer();
+  sharp(fs.readFileSync(path)).resize({ height, width, fit: fit }).toBuffer();
 
 export const getRandomName = (bytes = 16) =>
   crypto.randomBytes(bytes).toString("hex");
