684c4f8c3303a01265b8e54a031322c007cde7d4
RichardPickman <drezzerock@gmail.com>
2023-02-11 20:41:50 +0100

    feat: add new data for model

--- model/schemes/Quiz.ts
+++ model/schemes/Quiz.ts
@@ -1,10 +1,17 @@
 import mongoose from "mongoose";
 import { Schema } from "mongoose";
 
-const quizSchema = new Schema({
-  questions: [{ type: Schema.Types.ObjectId, ref: "questions" }],
-  title: String,
-});
+const quizSchema = new Schema(
+  {
+    questions: [{ type: Schema.Types.ObjectId, ref: "questions" }],
+    author: { type: Schema.Types.ObjectId, ref: "users" },
+    title: String,
+    image: String,
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const quizModel = mongoose.model("quiz", quizSchema);
 
